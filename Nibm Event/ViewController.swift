b97afd2f18dd29f8c00b02c66452ff512b3fc130
supun srilal kumara <supunsrilal246@gmail.com>
2020-02-28 22:38:13 +0530

    Initial Commit

---
+++ Nibm Event/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Nibm Event
+//
+//  Created by Supun Srilal on 2/28/20.
+//  Copyright © 2020 Macbook Pro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
